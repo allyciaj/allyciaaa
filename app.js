aefc346f226f79f5f94fef82802c509264f5b6f5
allyciaj <165746271+allyciaj@users.noreply.github.com>
2024-04-02 13:23:36 +0800

    app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Allycia Joshin - 51021001 - Sistem Informasi\n')
 })
 
 app.listen(PORT, HOST)
